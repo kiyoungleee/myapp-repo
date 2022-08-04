912750097f9851f3c8429b8492a9e61a3c7653f3
kiyoungleee <107023252+kiyoungleee@users.noreply.github.com>
2022-08-04 15:04:09 +0900

    Update Layout.js

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -38,7 +38,7 @@ export default function Layout() {
                 <HomeIcon />
               </IconButton>
               <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                ExamApp
+                T2 Finish!!
               </Typography>
             </Toolbar>
           </AppBar>
